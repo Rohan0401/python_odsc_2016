62e9fe549d7a7bcb48e5cfb4a65737593c17788f
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2014-10-30 18:05:50 +0100

    unsupervised learning

--- notebooks/solutions/04_svm_rf.py
+++ notebooks/solutions/04_svm_rf.py
@@ -2,31 +2,41 @@
 from sklearn.svm import SVC
 from sklearn import metrics
 
+
+fig = plt.figure(figsize=(6, 6))
+
+i = 0
 for kernel in ['rbf', 'linear']:
     clf = SVC(kernel=kernel).fit(Xtrain, ytrain)
     ypred = clf.predict(Xtest)
-    print("SVC: kernel = {0}".format(kernel))
-    print(metrics.f1_score(ytest, ypred))
-    plt.figure()
-    plt.imshow(metrics.confusion_matrix(ypred, ytest),
+    label = "SVC: kernel = {0}".format(kernel)
+    print('{0}: {1}'.format(label, metrics.f1_score(ytest, ypred))
+    ax = fig.add_subplot(3, 2, i + 1, xticks=[], yticks=[])
+    ax.imshow(metrics.confusion_matrix(ypred, ytest),
                interpolation='nearest', cmap=plt.cm.binary)
-    plt.colorbar()
-    plt.xlabel("true label")
-    plt.ylabel("predicted label")
-    plt.title("SVC: kernel = {0}".format(kernel))
-    
+    #ax.colorbar()
+    ax.set_xlabel("true label")
+    ax.set_ylabel("predicted label")
+    ax.set_title(label)
+    i += 1
+
 # random forest results
 from sklearn.ensemble import RandomForestClassifier
 
-for max_depth in [3, 5, 10]:
-    clf = RandomForestClassifier(max_depth=max_depth).fit(Xtrain, ytrain)
-    ypred = clf.predict(Xtest)
-    print("RF: max_depth = {0}".format(max_depth))
-    print(metrics.f1_score(ytest, ypred))
-    plt.figure()
-    plt.imshow(metrics.confusion_matrix(ypred, ytest),
-               interpolation='nearest', cmap=plt.cm.binary)
-    plt.colorbar()
-    plt.xlabel("true label")
-    plt.ylabel("predicted label")
-    plt.title("RF: max_depth = {0}".format(max_depth))
+for max_features in [1.0, 0.3]:
+    for n_estimators in [10, 100]:
+        clf = RandomForestClassifier(max_features=max_features,
+                                     n_estimators=n_estimators,
+                                     random_state=0).fit(Xtrain, ytrain)
+        ypred = clf.predict(Xtest)
+        label = "RF: max_features = {0}, n_estimators = {1}".format(
+            max_features, n_estimators)
+        print('{0}: {1}'.format(label, metrics.f1_score(ytest, ypred))
+        ax = fig.add_subplot(3, 2, i + 1, xticks=[], yticks=[])
+        ax.imshow(metrics.confusion_matrix(ypred, ytest),
+                   interpolation='nearest', cmap=plt.cm.binary)
+        #ax.colorbar()
+        ax.set_xlabel("true label")
+        ax.set_ylabel("predicted label")
+        ax.set_title(label)
+        i += 1
---
+++ notebooks/solutions/05_eigen_faces.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.decomposition import PCA
+
+faces = fetch_olivetti_faces()
+
+X = faces.data
+pca = PCA(n_components=100).fit(X)
+
+# set up the figure
+fig = plt.figure(figsize=(6, 6))  # figure size in inches
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+eigen_faces = pca.components_.reshape((pca.components_.shape[0], 64, 64))
+
+# plot the eigen faces:
+for i in range(16):
+    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+    ax.imshow(eigen_faces[i], cmap=plt.cm.bone, interpolation='nearest')
+
+
+# plot the reconstruction
+
+# compute embedding
+X_emb = pca.transform(X)
+
+# compute reconstruction
+X_rec = np.dot(X_emb, pca.components_).reshape((X_rec.shape[0], 64, 64))
+
+# set up the figure
+fig = plt.figure(figsize=(6, 6))  # figure size in inches
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+# plot the faces:
+for i in range(0, 16, 2):
+    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+    ax.imshow(faces.images[i], cmap=plt.cm.bone, interpolation='nearest')
+    ax = fig.add_subplot(4, 4, i + 2, xticks=[], yticks=[])
+    ax.imshow(X_rec[i], cmap=plt.cm.bone, interpolation='nearest')
